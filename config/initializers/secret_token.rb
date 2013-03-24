ad96c2ca9ff9838daea9e92e558368663339b1d7
Keith Lee <keithlee@gmail.com>
2013-03-24 10:53:47 -0700

    Rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spoon::Application.config.secret_token = '836ccd45f6b010a870f8279461f81db1b01a563fba4bd966214bbca2c4ef749de20fc69759d79e243893e2a71b241dde9aa13e2d168b7d1d6663d68cb37ebb50'
